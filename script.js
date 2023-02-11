ed7983855e193eb7a828301fdb75c5043ef8b1ef
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-02-11 13:13:40 +0530

    commit

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((student)=>{
+    if(student.marks>50){
+      console.log(student);
+    }
+  })
+
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student)=>{
+    if(student.marks>50){
+      console.log(student);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
-}
 
+let user4=[];
+user4.unshift({id:4,name:"aman",age:"20",marks:85});
+console.log(user4);
+}
 function removeFailedStudent() {
   //Write your code here, just console.log
+   let index=0;
+   let marks=[];
+  if(marks<50){
+    arr[index].pop();
+     console.log(arr);
+      index++;
+  }
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  //Write your code here
+  let aman = [
+    { id: 5, name: "rohan", age: "18",marks:51  },
+    { id: 6, name: "mohan", age: "24",marks:71 },
+    { id: 7, name: "kishan", age: "22",marks:41 },
+  ];
+  let ayushsir=arr.concat(aman);
+  console.log(ayushsir);
 }
